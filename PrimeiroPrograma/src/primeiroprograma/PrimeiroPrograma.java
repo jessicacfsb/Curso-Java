c7634e9e145a047d2ad942429b861a7075f2fc8e
jessicacfsb <jessica.cfsb@gmail.com>
2021-10-19 21:47:13 -0300

    Primeiro Programa em Java

---
+++ PrimeiroPrograma/src/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package primeiroprograma;
+
+/**
+ *
+ * @author const
+ */
+public class PrimeiroPrograma {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         
+    }
+    
+}
